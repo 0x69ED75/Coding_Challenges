14cb250be222b44edcaf7fb6b08ccd564ca98d80
Logan <cb58logan@gmail.com>
2022-06-03 21:21:55 +0100

    CodeWar challenges completed on 3/6/22

--- src/persistence.java
+++ src/persistence.java
@@ -1,16 +1,20 @@
 //https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec
 public class persistence {
     public static void main(String[] args) {
-        System.out.println(999);
+        System.out.println(persist(39));
     }
-    public static int persistence(int n) {
-        int persist = 999;
-        int x = 0;
-            char[] digits = String.valueOf(n).toCharArray();
-            for (int i = 1; i < digits.length; i++){
-                x = digits[i] * digits[i-1];
-            n = x;
+
+    public static long persist(long n) {
+        int count = 0;
+        while (String.valueOf(n).length() > 1 ) {
+            int product = 1;
+            int[] y = String.valueOf(n).chars().map(Character::getNumericValue).toArray(); // getting the string equivalent of n to turn into chars which turn into their integer equivalents, stored in an array
+            for (int j : y) {
+                product = product * j;
+            }
+            n = product;
+            count++;
         }
-        return n;
+        return count;
     }
 }
--- src/sum.java
+++ src/sum.java
@@ -1,9 +1,10 @@
+//https://www.codewars.com/kata/55f2b110f61eb01779000053
 import java.util.stream.IntStream;
 
 public class sum {
 
     public static void main(String[] args) {
-        System.out.println(GetSum(0,-1));
+        System.out.println(GetSum(9,11));
     }
 
     public static int GetSum(int a, int b)
