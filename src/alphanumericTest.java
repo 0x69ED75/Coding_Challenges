58d38021e76eb9a8b3f1591816a37e165b4a9a19
Logan <cb58logan@gmail.com>
2022-06-21 17:51:00 +0100

    problems completed on 21/6/22

---
+++ src/alphanumericTest.java
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/526dbd6c8c0eb53254000110
+public class alphanumericTest {
+    public static void main(String[] args) {
+        System.out.println(alphanumeric("abcdefghijklmnopq."));
+    }
+    public static boolean alphanumeric(String s){
+        if(s.length() == 0){ return false;}
+        String x = s.replaceAll("[^a-zA-Z0-9]", " ");
+        return x.equals(s.trim());
+    }
+}
