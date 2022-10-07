df4f8f8b1f1328e1d5fdcc6fd344df15588b1930
logan <cb58logan@gmail.com>
2022-10-07 17:51:17 +0100

    Leetcode Challenge completed 7/10/22

---
+++ src/Leetcode/Java_2_linkedList_add_two_num.java
@@ -0,0 +1,25 @@
+/*
+https://leetcode.com/problems/add-two-numbers/description/
+Given two linked list repsenting two non-negative integers, where the digits are stored in reverse order
+Add the two numbers, and return the sum as a linked list.
+ */
+package Leetcode;
+
+public class Java_2_linkedList_add_two_num {
+//    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//        int hold = 0;
+//        ListNode x = new ListNode(0);
+//        ListNode y = x;
+//        while(hold != 0 || l1 != null || l2 != null){
+//            int l1Val = (l1 != null) ? l1.val : 0;
+//            int l2Val = (l2 != null) ? l2.val : 0;
+//            int val = l1Val + l2Val + hold;
+//            hold = val/10;
+//            y.next = new ListNode(val%10);
+//            y = y.next;
+//            if(l1 != null){l1 = l1.next;}
+//            if(l2 != null){l2 = l2.next;}
+//        }
+//        return x.next;
+//    }
+}
