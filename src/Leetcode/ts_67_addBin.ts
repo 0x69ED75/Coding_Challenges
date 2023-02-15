0a72f6096e07b1806748c770ba3036aec87cf451
Logan <cb58logan@gmail.com>
2023-02-15 00:28:12 +0000

    better addBin solution

--- src/Leetcode/ts_67_addBin.ts
+++ src/Leetcode/ts_67_addBin.ts
@@ -3,6 +3,31 @@ https://leetcode.com/problems/add-binary/description/
 Given two binary strings a and b, return their sum as a binary string.
  */
 function addBinary(a: string, b: string): string {
+    return toBinary((BigInt(`0b${a}`)+BigInt(`0b${b}`)))
+    }
+
+function toBinary(sum:bigint): string{
+    let curr:bigint[] = []
+    while(sum >= 1){
+        curr.push((sum % BigInt(2)))
+        sum = sum /= BigInt(2)
+    }
+    let str:string = ""
+    while(curr.length > 0){
+        str += curr.pop()
+    }
+    return str.length === 0 ? "0" : str
+}
+
+
+
+console.log(addBinary("10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101"
+                     ,"110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"))
+
+/*
+Solution fails because test gives numbers of a massive size which not even bigint supports, but would work otherwise
+
+function addBinary(a: string, b: string): string {
     return toBinary(toInteger(a) + toInteger(b))
 };
 
@@ -30,7 +55,4 @@ function toBinary(sum:number): string{
     }
     return str.length === 0 ? "0" : str
 }
-
-console.log(addBinary("1010"
-                     ,"1011"))
-
+ */
